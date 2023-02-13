b11121f81d3eefbb5caa9d0a8d6e4b57f688f30a
jcolicchio-soundag <82787353+jcolicchio-soundag@users.noreply.github.com>
2023-02-13 15:56:57 -0800

    Revisions based on comments from Michael

--- sounDMR2/R/Sound_DMRv2.R
+++ sounDMR2/R/Sound_DMRv2.R
@@ -1301,24 +1301,24 @@ generate_zoomframe <- function(gene_cord_df, MFrame , Gene_col="Gene_name", filt
 }
                                 
                               
-#' boot_score
+boot_score
 #' @description
-#' Bootstrap analysis comparing target region with other regions of gewnome
+#' Bootstrap analysis comparing target region with other regions of genome
 #'
-#' @param SS_Obj (list) - Aggregated Changepoint Object created from sound_score
+#' @param sound_score_obj (list) - Aggregated Changepoint Object created from sound_score
 #' @param target_gene (str) - name of the target gene that was hit with oligos
 #' @param target_start (str) - Start position relative to gene start that was hit with oligos
 #' @param target_end (str) - Stop position relative to gene start that was hit with oligos
 #' @param nboots (str) - Number of bootstrap replicates
-#' @param nscore (str) - Name of the score column to run bootstrapping on (one of dmr_score or dmr_score2)
-#' @return Boot_Object (list) - File that contains information updated on target region info and bootstrap values
+#' @param scoring_col_name (str) - Name of the score column to run bootstrapping on (one of dmr_score or dmr_score2)
+#' @return Boot_Obj (list) - File that contains information updated on target region info and bootstrap values
 #' @export
 
 
-boot_score<-function(SS_Obj = Score_Out, target_gene="Glyma.07G034800", target_start=-1000, target_end=0, nboots=1000, score="dmr_score"){
+boot_score<-function(sound_score_obj = NA, target_gene= NA, target_start=-1000, target_end=0, nboots=1000, scoring_col_name ="dmr_score"){
   #Make necesarry objects
-  SS_Obj$region_summary->rs
-  SS_Obj$methyl_summary->ms
+  sound_score_obj$region_summary->rs
+  sound_score_obj$methyl_summary->ms
   rs[rs$Gene==target_gene,]->target_rs
   
   #create data frame to be filled with results of bootstrapping
@@ -1327,12 +1327,12 @@ boot_score<-function(SS_Obj = Score_Out, target_gene="Glyma.07G034800", target_s
   #provide column names
   colnames(boot_out) <- c('Gene', 'CG_Score', 'CHG_Score', "CHH_Score", 'Target')
   
-  #Calculate precision adjusted score
+  #Calculate precision adjusted score for each change point region
   target_rs %>%
     group_by(cp_group) %>%
     mutate(distance_from_target= min(c(abs(Start-target_start), abs(Start-target_end),abs(Stop-target_start), abs(Stop-target_end))),
            distance_from_target=ifelse(Start<=target_start & Stop >= target_end, 0, distance_from_target),
-           adjusted_soundscore= ifelse(distance_from_target>10000,0,(1-(0.0001*distance_from_target))*!!as.name(score))) -> target_rs
+           adjusted_soundscore= ifelse(distance_from_target>10000,0,(1-(0.0001*distance_from_target))*!!as.name(scoring_col_name))) -> target_rs
   
   
   #Find strongest DMR around target gene
@@ -1340,11 +1340,11 @@ boot_score<-function(SS_Obj = Score_Out, target_gene="Glyma.07G034800", target_s
   max(target_rs$adjusted_soundscore[target_rs$CX=="CHG"])->boot_out$CHG_Score[1]
   max(target_rs$adjusted_soundscore[target_rs$CX=="CHH"])->boot_out$CHH_Score[1]
   
-  # Write info to boot_out file
+  # Write info to Boot_Object
   1->boot_out$Target[1]
   target_gene->boot_out$Gene[1]
   
-  # Make non-target file
+  # Make non-target frame
   ms[ms$Gene!=target_gene,]->nontarget_ms
   
   # Create lookup table with info for each gene
@@ -1360,10 +1360,10 @@ boot_score<-function(SS_Obj = Score_Out, target_gene="Glyma.07G034800", target_s
   #subset to only include these rows
   nontarget_ms[nontarget_ms$far_enough==TRUE,]->nontarget_ms_good_distance
   
-  #sample from rows
-  sample_n(nontarget_ms_good_distance, nboots)->boot_positions
+  # sample from rows
+  sample_n(nontarget_ms_good_distance, nboots, replace=TRUE)->boot_positions
   
-  #run bootstrapping
+  # run bootstrapping
   for(i in 1:nrow(boot_positions)){
     rs[rs$Gene==boot_positions$Gene[i],]->boot_rs
     boot_positions$Zeroth_pos[i]->target_start
@@ -1372,9 +1372,9 @@ boot_score<-function(SS_Obj = Score_Out, target_gene="Glyma.07G034800", target_s
       group_by(cp_group) %>%
       mutate(distance_from_target= min(c(abs(Start-target_start), abs(Start-target_end),abs(Stop-target_start), abs(Stop-target_end))),
              distance_from_target=ifelse(Start<=target_start & Stop >= target_end, 0, distance_from_target),
-             adjusted_soundscore= ifelse(distance_from_target>10000,0,(1-(0.0001*distance_from_target))*!!as.name(score))) -> boot_rs
+             adjusted_soundscore= ifelse(distance_from_target>10000,0,(1-(0.0001*distance_from_target))*!!as.name(scoring_col_name ))) -> boot_rs
     
-    #Save bootstrap dmr_Scores
+    # Write info to Boot_Object
     
     max(boot_rs$adjusted_soundscore[boot_rs$CX=="CG"])->boot_out$CG_Score[i+1]
     max(boot_rs$adjusted_soundscore[boot_rs$CX=="CHG"])->boot_out$CHG_Score[i+1]
