dfa19be77cac940e1a959b3ebe80b7d0e1ea3027
Sebastian Bassi <sebastian@toyoko.io>
2023-10-15 01:26:17 -0700

    Update splitbychunkw.R (bugfix)

--- Docker/splitbychunkw.R
+++ Docker/splitbychunkw.R
@@ -3,6 +3,6 @@ args <- commandArgs(trailingOnly = TRUE)
 
 
 library(sounDMR)
-chuncksize <- as.integer(args[3])
-result <- sounDMR::split_by_chunk(args[1], args[2], chuncksize)
+chuncksize <- as.integer(args[2])
+result <- sounDMR::split_by_chunk(args[1], chuncksize, args[3])
 print(result)
