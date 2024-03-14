0227ee49d68f7e323aa8aa9c34c13b35f307d64a
Sebastian Bassi <sebastian@toyoko.io>
2024-03-14 06:07:56 +0000

    using pattern matching

--- Docker/step4_sounDMRw.R
+++ Docker/step4_sounDMRw.R
@@ -12,11 +12,11 @@ spec = "
 This creates a dmr object and methyl syummary. the methyl summary will be saved in the directory.  
 This will be applied over all directories inside given directory.
 
-Usage: Rscript SounDMR_step3.R [options]
+Usage: Rscript step4_sounDMRw.R [options]
 
 Options:
   <directory=s> Directory path for the methylframe/megaframe. Hint:chunks
-  <Megaframe_suffix=s> Suffix of megaframe file.
+  <Megaframe_pattern=s> Pattern of megaframe file.
   <exp_design=s> Input experimental design file. 
   <control_value=s> Default is C. This takes in the variable for control in the additional summary column. For example if your Group is T vs C, then control column would be C and treated column will be T, additional_sum_col will be 'Group'
   <treated_value=s> Default is T. This takes in the variable for control in the additional summary column.
@@ -29,33 +29,37 @@ Contact: name@address
 "
 GetoptLong(spec, template_control = list(opt_width = 23))
 
+print(directory)
+print(exp_design)
+
 experimental_design_df <- fread(exp_design, header = TRUE, sep=",")
 
 base_dir <- directory
 subdirs <- list.dirs(base_dir, full.names = TRUE, recursive = FALSE)
 process_subdir <- function(subdir) {
-  print(subdir)
-  
+  mf <- paste(subdir, "megaframe", sep="/")
+  # mf: /ftmp/batch1_4/chr_chr1/chunks/chunk1_0_250000/megaframe
   groups <- strsplit(subdir, split = "/")
   chrmall <- groups[[1]][6]
+  # chrmall: chunk1_0_250000
   chk <- groups[[1]][4]
+  # chk: chr_chr1
   chrm <- gsub("chr_", "", chk)
   file_pre <- paste(file_prefix, chrm, chrmall, sep = "_")
-  
-  setwd(subdir)
-  
-  #Sebastian_to fix
-  #File to choose for test: /efs/dmr/batch1-4/chr_chr1/chunks/chunk1_0_250000/megaframe/chunk1_0_250000_chr1_B1_4_combined_MegaFrame.csv
-  Methylframe <- fread(paste('*',Megaframe_suffix), header=TRUE, sep=",")
+  # file_pre: FP_chr1_chunk1_0_250000
+  setwd(mf)
+  if (length(Sys.glob(Megaframe_pattern, dirmark = FALSE))>1) {
+  stop("There is more than one pattern matching for Megaframe_pattern")
+  }
+  Methylframe <- fread(Sys.glob(Megaframe_pattern, dirmark = FALSE)[1], header=TRUE, sep=",")
   #create dmrobj
-  dmr_obj <- soundMR::create_dmr_obj(Methylframe, experimental_design_df)
+  dmr_obj <- sounDMR::create_dmr_obj(Methylframe, experimental_design_df)
   #create methyl summary
   methyl_summary <- soundMR::create_methyl_summary(dmr_obj, 
                                           control = control_value, 
                                           treated = treated_value,
                                           additional_summary_cols = list(
-                                            c(stat, additional_sum_col)))
-  
+                                            c(stat, additional_sum_col)))  
   
   write.csv(methyl_summary,paste(file_pre,"methyl_sum.csv", sep="_"), row.names = F)
   rm(dmrobj,methyl_summary)
